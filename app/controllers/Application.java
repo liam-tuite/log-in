923f6d0ec02a78f2a666f920e99a604a9146f0e5
Vasily Ulianko <vulianko@vsware.ie>
2018-07-26 16:22:23 +0100

    Send form data to the controller

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -2,11 +2,27 @@ package controllers;
 
 import play.mvc.*;
 import views.html.*;
+import play.data.*;
+import model.*;
+
+import javax.inject.Inject;
 
 public class Application extends Controller {
 
+	@Inject
+    private play.data.FormFactory formFactory;
+
 	public Result login(){
 		//TODO Verify user login and direct to next page if successful
-		return ok(login.render());
+		Form<User> userForm = formFactory.form(User.class);
+
+		return ok(login.render(userForm));
+	}
+
+	public Result loginPost() {
+		Form<User> userForm = formFactory.form(User.class);
+		User user = userForm.bindFromRequest().get();
+		System.out.println("Hello " + user.getUsername());
+        return ok("Hello " + user.getUsername());
 	}
 }
---
+++ app/model/User.java
@@ -0,0 +1,24 @@
+package model;
+
+public class User {
+
+    protected String username;
+    protected String password;
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+}
